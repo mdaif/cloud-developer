8b4b569b486bfebac47d0c8e40eb41d469501a28
Mohamed Daif <daif.control@gmail.com>
2020-04-10 18:13:12 +0200

    add createToDO

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '7s75gpnyjd'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'daif.eu.auth0.com',            // Auth0 domain
+  clientId: 'q9ZDfmXELzGEhJoy63eYBxbZA1zHDfJh',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
