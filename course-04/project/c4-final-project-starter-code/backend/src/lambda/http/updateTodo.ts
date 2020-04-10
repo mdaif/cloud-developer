c97a4c53f52d2d1ca6c988ae71dd78a9bacbb261
Mohamed Daif <daif.control@gmail.com>
2020-04-10 23:00:54 +0200

    update todo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,44 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { getUserId } from '../utils'
 
-// import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+const docClient = new AWS.DynamoDB.DocumentClient()
+const ToDoTable = process.env.TODO_TABLE
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
-//   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-//   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-//   return undefined
-// }
+  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+  const updated = await docClient.update({
+      TableName: ToDoTable,
+      Key: {
+        userId,
+        todoId
+      },
+      UpdateExpression: "set #name = :name, dueDate=:dueDate, done=:done",
+      ExpressionAttributeValues:{
+        ":name": updatedTodo.name,
+        ":dueDate": updatedTodo.dueDate,
+        ":done": updatedTodo.done
+    },
+    ExpressionAttributeNames: {
+        "#name": "name"
+    },
+    ReturnValues:"UPDATED_NEW"
+  }).promise()
+  return {
+    statusCode: 200,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+        updated
+    })
+    }
+}
