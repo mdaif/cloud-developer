cd11ffeefd7d55eab0a5ee67ef20427f7d7b96bc
Mohamed Daif <daif.control@gmail.com>
2020-04-11 13:56:37 +0200

    fix update todo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -7,28 +7,43 @@ import { getUserId } from '../utils'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const ToDoTable = process.env.TODO_TABLE
+const ToDoIndex = process.env.TODO_INDEX
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const userId = getUserId(event)
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+  const todos = await docClient.query({
+    TableName: ToDoTable,
+    IndexName: ToDoIndex,
+    KeyConditionExpression: 'userId = :userId and todoId = :todoId',
+    ExpressionAttributeValues: {
+        ':userId': userId,
+        ':todoId': todoId
+      },
+    ScanIndexForward: false,
+    Limit : 1
+  }).promise()
+
+  const createdAt = todos.Items[0].createdAt
   const updated = await docClient.update({
       TableName: ToDoTable,
       Key: {
         userId,
-        todoId
+        createdAt
       },
+      ConditionExpression: "todoId = :todoId",
       UpdateExpression: "set #name = :name, dueDate=:dueDate, done=:done",
       ExpressionAttributeValues:{
         ":name": updatedTodo.name,
         ":dueDate": updatedTodo.dueDate,
-        ":done": updatedTodo.done
-    },
-    ExpressionAttributeNames: {
-        "#name": "name"
-    },
+        ":done": updatedTodo.done,
+        ":todoId": todoId
+      },
+      ExpressionAttributeNames: {
+          "#name": "name"
+      },
     ReturnValues:"UPDATED_NEW"
   }).promise()
   return {
