7d2c1fd7ca83a21226537e08c7ac4c443d894f17
Mohamed Daif <daif.control@gmail.com>
2020-04-12 17:32:16 +0200

    feat: return presigned url

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,33 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
+const s3 = new AWS.S3({
+    'signatureVersion': 'v4'
+})
+const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
+const imagesBucket = process.env.IMAGES_S3_BUCKET
 
-//   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-//   return undefined
-// }
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    const uploadUrl = getUploadUrl(todoId)
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+            uploadUrl
+        })
+    }
+}
+
+function getUploadUrl(todoId: String) {
+    return s3.getSignedUrl('putObject', {
+        Bucket: imagesBucket,
+        Key: todoId,
+        Expires: urlExpiration
+    })
+}
