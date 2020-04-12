98285e33d594089e74f95b290ff50494e1377499
Mohamed Daif <daif.control@gmail.com>
2020-04-12 16:38:54 +0200

    feat: delete todo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,51 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+import { getUserId } from '../utils'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
+const docClient = new AWS.DynamoDB.DocumentClient()
+const ToDoTable = process.env.TODO_TABLE
+const ToDoIndex = process.env.TODO_INDEX
 
-//   // TODO: Remove a TODO item by id
-//   return undefined
-// }
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
+  
+  const todo = await docClient.query({
+    TableName: ToDoTable,
+    IndexName: ToDoIndex,
+    KeyConditionExpression: 'userId = :userId and todoId = :todoId',
+    ExpressionAttributeValues: {
+        ':userId': userId,
+        ':todoId': todoId
+      },
+    ScanIndexForward: false,
+    Limit : 1
+  }).promise()
+  const createdAt = todo.Items[0].createdAt
+
+  const deleted = await docClient.delete({
+    TableName: ToDoTable,
+    Key: {
+      userId,
+      createdAt
+    },
+    ConditionExpression: "todoId = :todoId",
+    ExpressionAttributeValues:{
+      ":todoId": todoId
+    },
+  }).promise()
+  return {
+    statusCode: 200,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      'Deleted': deleted
+    })
+  }
+  
+
+}
